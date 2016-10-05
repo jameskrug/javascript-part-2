83edd16fc0a08ff4746a57d993a77a6ec3cf2560
James <krugger322@hotmail.com>
2016-10-05 19:12:34 +0000

    added function sort by person name length and alphabetical by email

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+/*
 var longerString = ["zabc", "dfdsafe", "fdfsafdsafdses", "fdsas"];
 var sortedString = [];
 
@@ -16,4 +17,65 @@ function sortString(str1, str2){
 longerString.sort(sortString);
 
 console.log(longerString);
+*/
+
+var nameAndEmails = []
+    var person1 = {
+        name: "jimmyson",
+        email: "zzjim@rogers.com"
+    }
+    var person2 = {
+        name:"bill",
+        email: "FFbill@rogers.com"
+    }
+    var person3 = {
+        name: "steve",
+        email: "steve_wakowski@stuff.ca"
+    }
+    var person4 = {
+        name: "franklinwilliams",
+        email: "FFAfrank_williamson@gs.ca"
+    }
+
+    nameAndEmails.push(person1);
+    nameAndEmails.push(person2);
+    nameAndEmails.push(person3);
+    nameAndEmails.push(person4);
+
+function sortString(str1, str2){
+    if (str1.name.length > str2.name.length){
+        return 1;
+    }
+    else if (str1.name.length > str2.name.length){
+        return -1;
+    }
+    else {
+        return 0;
+    }
+}
+
+nameAndEmails.sort(sortString);
+
+console.log("sorted by name length: ");
+console.log(nameAndEmails);
+
+function sortEmailAlphabetically(str1, str2){
+    var upperStr1 = str1.email.toUpperCase();
+    var upperStr2 = str2.email.toUpperCase();
+    if (upperStr1 > upperStr2){
+        return 1;
+    }
+    else if (upperStr1 > upperStr1){
+        return -1;
+    }
+    else {
+        return 0;
+    }
+
+}
+
+nameAndEmails.sort(sortEmailAlphabetically);
+
+console.log("sorted alphabetically by email: ");
+console.log(nameAndEmails);
 
